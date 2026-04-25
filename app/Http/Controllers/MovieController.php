5a98d4fd6d105fe1c61afa013f0de2f9eb1d4c05
Inayah Henni <inayahenni@gmail.com>
2026-04-25 16:17:07 +0700

    refactor: perbarui controller, request, model, dan routes

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,143 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Movie;
-use App\Models\Category;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\StoreMovieRequest;
+use App\Http\Requests\UpdateMovieRequest;
+use App\Services\MovieService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MovieController extends Controller
 {
+    public function __construct(protected MovieService $movieService) {}
 
-    public function index()
+    public function index(Request $request): View
     {
+        $movies = $this->movieService->getHomepageMovies($request->query('search'));
 
-        $query = Movie::latest();
-        if (request('search')) {
-            $query->where('judul', 'like', '%' . request('search') . '%')
-                ->orWhere('sinopsis', 'like', '%' . request('search') . '%');
-        }
-        $movies = $query->paginate(6)->withQueryString();
         return view('homepage', compact('movies'));
     }
 
-    public function detail($id)
+    public function show(string $id): View
     {
-        $movie = Movie::find($id);
+        $movie = $this->movieService->getMovieDetail($id);
+
         return view('detail', compact('movie'));
     }
 
-    public function create()
+    public function create(): View
     {
-        $categories = Category::all();
+        $categories = $this->movieService->getCategories();
+
         return view('input', compact('categories'));
     }
 
-    public function store(StoreMovieRequest $request)
+    public function store(StoreMovieRequest $request): RedirectResponse
     {
-        // Ambil data yang sudah tervalidasi
-        $validated = $request->validated();
+        $this->movieService->createMovie($request->validated());
 
-        // Simpan file foto jika ada
-        if ($request->hasFile('foto_sampul')) {
-            $validated['foto_sampul'] = $request->file('foto_sampul')->store('movie_covers', 'public');
-        }
-
-        // Simpan data ke database
-        Movie::create($validated);
-
-        return redirect('/')->with('success', 'Film berhasil ditambahkan.');
+        return redirect()->route('movies.index')
+            ->with('success', 'Film berhasil ditambahkan.');
     }
 
-
-    public function data()
+    public function data(): View
     {
-        $movies = Movie::latest()->paginate(10);
+        $movies = $this->movieService->getAdminMovies();
+
         return view('data-movies', compact('movies'));
     }
 
-    public function form_edit($id)
+    public function edit(string $id): View
     {
-        $movie = Movie::find($id);
-        $categories = Category::all();
+        $movie = $this->movieService->getMovieDetail($id);
+        $categories = $this->movieService->getCategories();
+
         return view('form-edit', compact('movie', 'categories'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdateMovieRequest $request, string $id): RedirectResponse
     {
-        // Validasi data
-        $validator = Validator::make($request->all(), [
-            'judul' => 'required|string|max:255',
-            'category_id' => 'required|integer',
-            'sinopsis' => 'required|string',
-            'tahun' => 'required|integer',
-            'pemain' => 'required|string',
-            'foto_sampul' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
+        $this->movieService->updateMovie($id, $request->validated());
 
-        // Jika validasi gagal, kembali ke halaman edit dengan pesan kesalahan
-        if ($validator->fails()) {
-            return redirect("/movies/edit/{$id}")
-                ->withErrors($validator)
-                ->withInput();
-        }
-
-        // Ambil data movie yang akan diupdate
-        $movie = Movie::findOrFail($id);
-
-        // Jika ada file yang diunggah, simpan file baru
-        if ($request->hasFile('foto_sampul')) {
-            $randomName = Str::uuid()->toString();
-            $fileExtension = $request->file('foto_sampul')->getClientOriginalExtension();
-            $fileName = $randomName . '.' . $fileExtension;
-
-            // Simpan file foto ke folder public/images
-            $request->file('foto_sampul')->move(public_path('images'), $fileName);
-
-            // Hapus foto lama jika ada
-            if (File::exists(public_path('images/' . $movie->foto_sampul))) {
-                File::delete(public_path('images/' . $movie->foto_sampul));
-            }
-
-            // Update record di database dengan foto yang baru
-            $movie->update([
-                'judul' => $request->judul,
-                'sinopsis' => $request->sinopsis,
-                'category_id' => $request->category_id,
-                'tahun' => $request->tahun,
-                'pemain' => $request->pemain,
-                'foto_sampul' => $fileName,
-            ]);
-        } else {
-            // Jika tidak ada file yang diunggah, update data tanpa mengubah foto
-            $movie->update([
-                'judul' => $request->judul,
-                'sinopsis' => $request->sinopsis,
-                'category_id' => $request->category_id,
-                'tahun' => $request->tahun,
-                'pemain' => $request->pemain,
-            ]);
-        }
-
-        return redirect('/movies/data')->with('success', 'Data berhasil diperbarui');
+        return redirect()->route('movies.data')
+            ->with('success', 'Data berhasil diperbarui.');
     }
 
-    public function delete($id)
+    public function destroy(string $id): RedirectResponse
     {
-        $movie = Movie::findOrFail($id);
+        $this->movieService->deleteMovie($id);
 
-        // Delete the movie's photo if it exists
-        if (File::exists(public_path('images/' . $movie->foto_sampul))) {
-            File::delete(public_path('images/' . $movie->foto_sampul));
-        }
-
-        // Delete the movie record from the database
-        $movie->delete();
-
-        return redirect('/movies/data')->with('success', 'Data berhasil dihapus');
+        return redirect()->route('movies.data')
+            ->with('success', 'Data berhasil dihapus.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,11 @@
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', [MovieController::class, 'index']);
-Route::get('/movie/{id}', [MovieController::class, 'detail']);
-Route::get('/movies/create', [MovieController::class, 'create']);
-Route::post('/movies/store', [MovieController::class, 'store']);
-Route::get('/movies/data', [MovieController::class, 'data']);
-Route::get('/movies/edit/{id}', [MovieController::class, 'form_edit']);
-
-Route::post('movies/{movie}/update', [MovieController::class, 'update'])->name('movies.update');
-Route::get('movies/delete/{id}', [MovieController::class, 'delete'])->name('movies.delete');
+Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
+Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+Route::get('/movies/data', [MovieController::class, 'data'])->name('movies.data');
+Route::get('/movies/{id}/edit', [MovieController::class, 'edit'])->name('movies.edit');
+Route::put('/movies/{id}', [MovieController::class, 'update'])->name('movies.update');
+Route::delete('/movies/{id}', [MovieController::class, 'destroy'])->name('movies.destroy');
