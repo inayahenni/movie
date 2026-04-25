0ae378c4d443432f7b82cce981d359a1a8b3d178
Inayah Henni <inayahenni@gmail.com>
2026-04-25 19:30:20 +0700

    fix: fix minor bugs

--- resources/views/form-edit.blade.php
+++ resources/views/form-edit.blade.php
@@ -2,9 +2,10 @@
 @section('title', 'Input Data Movie')
 @section('content')
     <h2 class="mb-4">Edit Movie</h2>
-    <form action="{{ route('movies.update', ['movie' => $movie->id]) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('movies.update', ['id' => $movie->id]) }}" method="POST" enctype="multipart/form-data">
         @csrf
-        @include('partials.movie-from-fields')
+        @method('PUT')
+        @include('partials.movie-form-fields')
         <div class="mb-3">
             <button type="submit" class="btn btn-primary">Simpan</button>
         </div>
--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <a href="/movies/data" class="btn btn-primary mt-4">List Movie</a>
     <h2 class="mb-4">Tambah Movie Baru</h2>
-    <form action="/movies/store" method="POST" enctype="multipart/form-data">
+    <form action="/movies" method="POST" enctype="multipart/form-data">
         @csrf
         @include('partials.movie-form-fields')
         <div class="mb-3">
